44419ed23c30d9bdeaa287c1838227b8147ea1f5
DanilBodrov <bodrov.da.a@gmail.com>
2025-05-23 13:21:12 +0300

    Initial commit

---
+++ src/main/java/org/example/districtdesignerservice/DistrictDesignerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.districtdesignerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrictDesignerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistrictDesignerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/districtdesignerservice/DistrictDesignerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.districtdesignerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistrictDesignerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
